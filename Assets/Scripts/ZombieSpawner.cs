02356c9aec35ffa273802a35beeeaa745e8d8ebc
honey1586 <58782737+honey1586@users.noreply.github.com>
2020-07-01 23:53:50 +0900

    zombie spawn start

---
+++ Assets/Scripts/ZombieSpawner.cs
@@ -0,0 +1,31 @@
+﻿using DigitalRuby.Pooling;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject obj = SpawningPool.CreateFromCache("Zombie_0");
+            if(obj == null)
+            {
+                Debug.LogErrorFormat("Unable to find object for key: {0}", "Zombie_0");
+            }
+
+            Vector3 pos = Random.onUnitSphere;
+            obj.transform.position = pos * UnityEngine.Random.Range(-5.0f, 5.0f);
+            obj.transform.rotation = Random.rotation;
+
+            obj.transform.SetParent(transform);
+        }
+    }
+}
